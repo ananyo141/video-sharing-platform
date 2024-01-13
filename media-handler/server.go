b84994f883fef9192f030ecfa352cb57c74ecddf
Ananyobrata Pal <ananyo141@gmail.com>
2024-01-13 13:18:27 +0530

    feat(media-go): add default and 404 routes, change route for playground and graphql

--- media-handler/server.go
+++ media-handler/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 
+	"encoding/json"
 	"log"
 	"media-handler/graph"
 	"media-handler/utils"
@@ -20,8 +21,29 @@ func main() {
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/playground", playground.Handler("GraphQL playground", "/graphql"))
+	http.Handle("/graphql", srv)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var success bool = true
+		var status int = http.StatusOK
+		var message string = "Media Handler API is running!"
+		if r.URL.Path != "/" {
+			success = false
+			status = http.StatusNotFound
+			message = "Please use a valid route"
+		}
+		response := map[string]interface{}{
+			"success": success,
+			"message": message,
+			"data": map[string]string{
+				"playground": "/playground",
+				"graphql":    "/graphql",
+			},
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(status)
+		json.NewEncoder(w).Encode(response)
+	})
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
