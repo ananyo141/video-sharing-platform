78eb350058ade3db2188b032b05c66da778ca32b
Ananyobrata Pal <ananyo141@gmail.com>
2024-01-13 12:07:12 +0530

    chore(media-go): rename database files; separate service file

---
+++ media-handler/db/init.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type DB struct {
+	client *mongo.Client
+}
+
+func Connect() *DB {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://media-db:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return &DB{
+		client: client,
+	}
+}
+
--- media-handler/db/video_service.go
+++ media-handler/db/video_service.go
@@ -1,47 +1,15 @@
-package db
+package db;
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	// "go.mongodb.org/mongo-driver/mongo/readpref"
 	"media-handler/graph/model"
 )
-
-type DB struct {
-	client *mongo.Client
-}
-
-func Connect() *DB {
-	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://media-db:27017")
-
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to MongoDB!")
-
-	return &DB{
-		client: client,
-	}
-}
 
 func (db *DB) GetVideo(id string) *model.Video {
 	videoCollec := db.client.Database("graphql-job-board").Collection("jobs")
