9472f0fc1c97111b9e71cc901283c68177959160
Ananyobrata Pal <ananyo141@gmail.com>
2024-02-26 13:40:05 +0530

    feat(video-express): configure rabbitmq with docker

--- video-processing/bucket-docker-entrypoint.sh
+++ video-processing/bucket-docker-entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 
 /app/wait-for-it.sh events-queue:5672 --timeout=30 --strict --
 
+# mc config host add miniobucket http://localhost:9000 minioadmin minioadmin
+# mc event add miniobucket/video-bucket arn:minio:sqs::PRIMARY:amqp --event put
+
 exec /usr/bin/minio "$@"
