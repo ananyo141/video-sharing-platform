e94b49d2444f258b89e3ae1bd7a03de740a725da
ankitaniket <ankitaniket3@gmail.com>
2024-03-30 20:17:43 +0530

    chore: Add createVideo function

--- web-client/src/components/ModalScreen.tsx
+++ web-client/src/components/ModalScreen.tsx
@@ -10,11 +10,8 @@ interface Props {
 const ModalScreen = ({ children, visible = true, onClose }: Props) => {
   return (
     <div
-      className={`h-screen z-50 w-full p-10 absolute top-0 left-0 flex items-center justify-center
-       ${
-        visible ? "" : "hidden"
-      } bg-black bg-opacity-80 z-20`}
-      onClick={onClose}
+      className={`h-full z-50 w-full p-10 absolute top-0 left-0 flex items-center justify-center
+       ${visible ? "" : "hidden"} bg-black bg-opacity-80 z-20`}
     >
       <div className="bg-white h-full w-full z-50 p-2 rounded shadow-xl">
         <div className="flex  justify-end">
--- web-client/src/middleware.ts
+++ web-client/src/middleware.ts
@@ -25,5 +25,5 @@ export default async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/login", "/register"],
+  matcher: ["/", "/login", "/register",],
 };
